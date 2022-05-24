61010c66713243a0a49e20d81438e21ef0d29248
chrisdhodges12 <102702357+chrisdhodges12@users.noreply.github.com>
2022-05-24 13:49:45 -0400

    display searched cities

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,41 @@
 var inputEl = document.getElementById("form-input");
 var searchButtonEl = document.getElementById("search-button");
 var cityButtonEl = document.getElementById("city-buttons")
+var cityHistory = JSON.parse(localStorage.getItem("city")) || [];
 
-var cityHistory = [];
 
 var apiKey =  "438ec97184afe633108bea6618c47685"
 
 
 searchButtonEl.addEventListener("click", function () {
-    var city = inputEl.value;
+    const city = inputEl.value;
+    cityHistory.push(city);
     localStorage.setItem("city", JSON.stringify(city));
-    
-    var cityButton = document.createElement("button");
-    cityButton.setAttribute("class", "city-buttons");
-    cityButton.innerHTML = city;
-    cityButtonEl.append(cityButton);
-    
-    
+    showHistory();
 
 })
 
-// function reset () {
-//     inputEl.innerHTML = "";
-// }
 
 function showHistory () {
-    cityButtonEl = JSON.parse(localStorage.getItem("city"));
+    cityButtonEl.innerHTML = "";
+    for (let i = 0; i < cityHistory.length; i++) {
+        var cityButton = document.createElement("input");
+        cityButton.setAttribute("type", "text");
+        cityButton.setAttribute("readonly", true);
+        cityButton.setAttribute("class", "city-buttons");
+        cityButton.setAttribute("value", cityHistory[i]);
+        cityButtonEl.append(cityButton);
+            cityButton.addEventListener("click", function () {
+                checkWeather(cityButton.value);
+            })
+        }
 
+}
+
+showHistory();
+
+function checkWeather() {
+    
 }
 
 // var formSubmitHandler = function(event) {
@@ -38,7 +47,19 @@ function showHistory () {
 //      cityEl.setAttribute("href", "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey);
 
 //      // append to container
-//
+//      cityEl.appendChild(cityButtonEl);
+
+//     // if (cityName) {
+    //     getCityData(cityName);
+    //     inputEl.value = "";
+    // } else {
+    //     alert("Please enter a city");
+    // }
+    // console.log(event);
+// };
+
+
+
 
 // var getCityData = function(city) {
 
@@ -46,4 +67,3 @@ function showHistory () {
 // }
 
 
-showHistory();
